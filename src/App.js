1776a5369702edb84943125b6f4987f7816d7246
abilnf <slin.lbeog@gmail.com>
2022-02-12 13:57:38 +0100

    Setup firebase dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOSDuweiGasQcUuaO2N8bSdC02-QVUxtk",
+  authDomain: "abilnf-odin-where-is-waldo.firebaseapp.com",
+  projectId: "abilnf-odin-where-is-waldo",
+  storageBucket: "abilnf-odin-where-is-waldo.appspot.com",
+  messagingSenderId: "102810098472",
+  appId: "1:102810098472:web:0794e726ab02bf3aa1ce1d",
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
